f30a7a5bc0cc9126b9e7c659840a26258f1e8c38
Florian Lubitz <florian@florianlubitz.de>
2017-07-20 15:20:14 +0200

    Finished

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
 $(function(){
+
+    const NUMJOCKER = 6;
+
+
+    var $panes = $('.pane');
+    var ids = new Array();
+    var randids = new Array();
+    for(var i = 0; i<40; i++){
+        var $pane = $panes[i]
+        ids[i] = parseInt($pane.id);
+    }
+
+    for(var i = 0; i<NUMJOCKER;i++){
+        randids[i] = ids[Math.floor(Math.random()*ids.length)];
+    }
+
+
+    console.log(randids);
+
+
     $('.dot').click(function(){
         $(this).addClass('opened');
         var id = $(this).parents('.row').data('id');
-        $('.pane#' + id).show();
+        $('.pane#' + id).addClass('open');
+
     });
 
     $('.close').click(function(){
         $(this).parents('.pane').hide();
+        console.log(randids.indexOf(parseInt($(this).parents('.pane').attr('id'))))
+        if(randids.indexOf(parseInt($(this).parents('.pane').attr('id'))) >= 0){
+            $('.joker').show();
+        }
+    });
+
+        $('.joker .close').click(function(){
+        $(this).parents('.joker').hide();
     });
 
     $('.show').click(function(){
-        $(this).children('.answer').show();
+        $(this).addClass('open');
+    });
+
+    $('.addpoints').click(function(){
+        var points = $(this).siblings('p').data('points');
+        var add = $(this).data('points');
+        console.log(points);
+        console.log(add);
+        $(this).siblings('p').data('points', points + add).html(points + add);
     });
 });
